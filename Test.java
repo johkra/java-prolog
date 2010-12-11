9c331ef66fc7ac8705a37c060b2275ef1f474b3a
Johannes Krampf <johkra@gmail.com>
2010-12-11 18:45:23 +0100

    Add tests for variables

--- Test.java
+++ Test.java
@@ -14,14 +14,29 @@ public class Test {
 		Atom a3 = new Atom("Jerry");
 		List<Term> head4 = new ArrayList<Term>();
 		head4.add(a3);
+		Atom a4 = new Atom("Micky");
+		List<Term> head6 = new ArrayList<Term>();
+		head6.add(a4);
+
+		Variable v1 = new Variable("X");
+		List<Term> head5 = new ArrayList<Term>();
+		head5.add(v1);
+		Variable v2 = new Variable("Y");
+		List<Term> head7 = new ArrayList<Term>();
+		head7.add(v2);
 
 		Fact cat = new Fact("cat", head);
 		Fact cat2 = new Fact("cat", head3);
 		Fact mouse = new Fact("mouse", head4);
+		Fact mouse3 = new Fact("mouse", head6);
+
+		Fact mouse2 = new Fact("mouse", head5);
+		Fact cat3 = new Fact("cat", head7);
 
 		Rules rules = new Rules();
 		rules.addRule(cat);
 		rules.addRule(mouse);
+		rules.addRule(mouse3);
 
 		test(cat, head);
 		test(cat2, head);
@@ -32,6 +47,9 @@ public class Test {
 		test(cat, head4);
 		test(mouse, head4);
 		test(mouse, head);
+
+		rules.is(mouse2, head5);
+		rules.is(cat3, head7);
 	}
 
 	public static void test(Fact f, List<Term> head) {
