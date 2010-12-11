b09d76d06b6a7528635ae80ed397989711408343
Johannes Krampf <johkra@gmail.com>
2010-12-11 18:43:00 +0100

    Implement Variables

--- Variable.java
+++ Variable.java
@@ -1,5 +1,16 @@
 public class Variable implements Term {
+	private String name;
+
 	public String getName() {
-		return "Variable";
+		return this.name;
+	}
+
+	public Variable(String name) {
+		this.name = name;
+	}
+
+	public boolean equals(Object o) {
+		return (o instanceof Variable) &&
+			((Variable)o).getName().equals(this.name);
 	}
 }
