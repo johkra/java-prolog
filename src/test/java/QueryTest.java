3776e6e8e3d3981f7aec4e5dc0515b74e5c74c1d
Johannes Krampf <johkra@gmail.com>
2011-03-06 13:46:44 +0100

    Extend tests

--- src/test/java/QueryTest.java
+++ src/test/java/QueryTest.java
@@ -1,12 +1,12 @@
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
 import java.io.PrintStream;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * User: Johannes Krampf <johkra@gmail.com>
@@ -20,62 +20,57 @@ public class QueryTest {
         System.setOut(new PrintStream(outContent));
     }
 
-    @Test
-    public void testSimpleQuery() throws Exception {
-        String commands = "boy(bill)\n" +
-                "boy(bill)?";
+    private void verifyOutput(String commands, String exceptedOutput) throws Exception {
         byte[] bytes = commands.getBytes("utf-8");
         ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-        Prolog.procFile(input,"");
-        assertEquals("Yes\n", outContent.toString());
+        Prolog.procFile(input, "");
+        assertEquals(exceptedOutput, outContent.toString());
+    }
+
+    @Test
+    public void testSimpleQuery() throws Exception {
+        verifyOutput("boy(bill)\n" +
+                "boy(bill)?",
+                "Yes\n");
     }
 
     @Test
     public void testVariableQuery() throws Exception {
-        String commands = "boy(X)?";
-        byte[] bytes = commands.getBytes("utf-8");
-        ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-        Prolog.procFile(input, "");
-        assertEquals("{X=bill}\n", outContent.toString());
+        verifyOutput("boy(X)?", "{X=bill}\n");
     }
 
     @Test
     public void testIndirectQuery() throws Exception {
-        String commands = "mother(alice,bill)\n" +
+        verifyOutput("mother(alice,bill)\n" +
                 "child(J,K) :- mother(K,J)\n" +
                 "son(X,Y) :- child(X,Y),boy(X)\n" +
-                "son(X,alice)?";
-        byte[] bytes = commands.getBytes("utf-8");
-        ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-        Prolog.procFile(input, "");
-        assertEquals("{X=bill}\n", outContent.toString());
+                "son(X,alice)?", "{X=bill}\n");
     }
 
     @Test
     public void testMember() throws Exception {
-       String commands = "member(X,[X|T])\n" +
-               "member(X,[H|T]) :- member(X,T)\n" +
-               "member(a,[a,b,c])?\n" +
-               "member(b,[a,b,c])?\n" +
-               "member(c,[a,b,c])?";
-        byte[] bytes = commands.getBytes("utf-8");
-        ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-        Prolog.procFile(input, "");
-        assertEquals("Yes\nYes\nYes\n", outContent.toString());
+        verifyOutput("member(X,[X|T])\n" +
+                "member(X,[H|T]) :- member(X,T)\n" +
+                "member(a,[a,b,c])?\n" +
+                "member(b,[a,b,c])?\n" +
+                "member(c,[a,b,c])?", "Yes\nYes\nYes\n");
     }
 
-        @Test
+    @Test
     public void testAppend() throws Exception {
-       String commands = "append([],L,L)\n" +
-               "append([X|A],B,[X|C]) :- append(A,B,C)\n" +
-               "append([a,b],[c,d],X)?";
-        byte[] bytes = commands.getBytes("utf-8");
-        ByteArrayInputStream input = new ByteArrayInputStream(bytes);
-        Prolog.procFile(input, "");
-        assertEquals("{X=[a,b,c,d]}\n", outContent.toString());
+        verifyOutput("append([],L,L)\n" +
+                "append([X|A],B,[X|C]) :- append(A,B,C)\n" +
+                "append([a,b],[c,d],X)?", "{X=[a,b,c,d]}\n");
     }
 
-
+    @Test
+    public void testLength() throws Exception {
+        verifyOutput("length([],0)\n" +
+                "length([H|T],N) :- length(T,Nt), N is Nt+1\n" +
+                "length([],X)?\n" +
+                "length([1],X)?\n" +
+                "length([1,2,3],X)?", "{X=0}\n{X=1}\n{X=3}\n");
+    }
 
     @After
     public void tearDown() throws Exception {
